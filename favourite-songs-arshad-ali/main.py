368a2e7cc1a3784a67d96d6f70e4914aab134cb9
Arshad Ali <arshadali.coder@gmail.com>
2025-06-11 15:31:34 +0530

    Update main.py

--- favourite-songs-arshad-ali/main.py
+++ favourite-songs-arshad-ali/main.py
@@ -7,4 +7,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Render will set this env var
+    app.run(host="0.0.0.0", port=port)
